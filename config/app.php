52bfd5346f3c1c2c9c71f7499a476366f1617e23
Paulo Reis <pauloreisprofissional@gmail.com>
2021-01-21 06:15:08 -0300

    Remoção do LaravelDebugar do App

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
